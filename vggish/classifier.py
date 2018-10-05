8237c14f613fa500d039d6776ab49eaf88f37491
liucijalat <liucija.lat@gmail.com>
2018-10-05 17:01:27 +0100

    fixed shuffling error and now get sensible (if poor) results

--- vggish/classifier.py
+++ vggish/classifier.py
@@ -1,3 +1,4 @@
+import numpy as np
 from numpy import genfromtxt
 np.set_printoptions(suppress=True)
 
@@ -7,13 +8,32 @@ from keras.callbacks import EarlyStopping
 
 from matplotlib import pyplot as plt
 
-X_train = genfromtxt("data/embs/X_train.csv", delimiter=',')
-y_train = genfromtxt("data/embs/y_train.csv", delimiter=',')
+# TODO: put the VGGish model as the bottom layers of KEras model rather than
+# have two separate processes
+# TODO: try with embeddings per second, rather than the sum over the whole clip
+# TODO: address the overfitting - dropout, regularization
+
+rng_state = np.random.get_state()
+rng_state
+
+# need to shuffle as positive and negative classes are ordered
+X_train = genfromtxt("data/embs/X_unbal.csv", delimiter=',')
+np.random.shuffle(X_train)
+y_train = genfromtxt("data/embs/y_unbal.csv", delimiter=',')
+np.random.set_state(rng_state)
+np.random.shuffle(y_train)
 X_train.shape
 y_train.shape
 
 X_eval = genfromtxt("data/embs/X_eval.csv", delimiter=',')
+np.random.set_state(rng_state)
+np.random.shuffle(X_eval)
 y_eval = genfromtxt("data/embs/y_eval.csv", delimiter=',')
+np.random.set_state(rng_state)
+np.random.shuffle(y_eval)
+
+X_train.shape
+y_train.shape
 X_eval.shape
 y_eval.shape
 
@@ -29,25 +49,25 @@ model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accurac
 
 
 callbacks = [
-    EarlyStopping(monitor='val_loss', verbose=2, patience=10),
+    EarlyStopping(monitor='val_loss', verbose=2, patience=5),
     ]
 
 history = model.fit(
      X_train,
      y_train,
      batch_size=batch_size,
-     epochs=100,  # redundant with callbacks
+     epochs=1000,  # redundant with callbacks
      validation_data=(X_eval, y_eval),
      shuffle=True,
      callbacks=callbacks)
 
-# In[9]:
+# In[1]:
 # Run all in one go to get the graph (Option+Shift+Enter)
+
 history_dict = history.history
 
 loss = history_dict['loss']
 val_loss = history_dict['val_loss']
-
 epochs = range(len(loss))
 
 plt.figure()
@@ -57,7 +77,7 @@ plt.title('Training and validation loss')
 plt.legend()
 plt.show()
 
-# In[9]:
+# In[2]:
 # Run all in one go to get the graph (Option+Shift+Enter)
 history_dict = history.history
 
