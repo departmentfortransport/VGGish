3f40d8e0c5e2c8e2d27950bd778279c18e4abda9
liucijalat <liucija.lat@gmail.com>
2018-10-05 17:00:40 +0100

    working code to obtain embeddings, and combine them to a single array

--- get_embeddings.py
+++ get_embeddings.py
@@ -1,12 +1,16 @@
 import numpy as np
+from numpy import genfromtxt
 from keras.models import Model
 from scipy.io import wavfile
 
+import os.path
 from os import listdir
 from os.path import isfile, join
 
 from vggish import vggish
 from vggish.preprocess_sound import preprocess_sound
+
+import time
 
 SEG_LEN = 5  # 5s
 #TODO: find out why would you want to cut the SEG_LEN
@@ -21,17 +25,14 @@ def get_vggish():
     model = Model(inputs=vggish_model.input,
                   outputs=vggish_model.get_layer("vggish_fc2").output)
 
-    return model, preprocess_sound
+    return model
 
 
-def get_embeddings(sound_file):
+def get_embeddings(model, sound_file):
     '''
     Obtain the (128,) embeddings for a given sound file by
     passing through VGGish
     '''
-
-    model, preprocessor = get_vggish()
-    # model.summary()
 
     sr, wav_data = wavfile.read(sound_file)
     # print(wav_data.shape)
@@ -45,17 +46,17 @@ def get_embeddings(sound_file):
     # cur_wav = wav_data[0:length]
     # cur_spectro = preprocess_sound(cur_wav, sr)
 
-    cur_spectro = preprocess_sound(wav_data, sr)
+    cur_spectro = preprocess_sound(wav_data, sr)  # take a while
     # print('cur_spectro.shape: ', cur_spectro.shape)
     cur_spectro = np.expand_dims(cur_spectro, 3)  # prediction doesn't work o/w
     # print('cur_spectro.shape: ', cur_spectro.shape)
 
-    result = model.predict(cur_spectro)
+    result = model.predict(cur_spectro)  # take a while
 
     # print(result)
     # print('result shape: ', result.shape)
 
-    result = np.sum(result, axis=0)
+    result = np.sum(result, axis=0)  # need to experiment with this
 
     # print('result: ', result)
     # print('result shape: ', result.shape)
@@ -63,7 +64,8 @@ def get_embeddings(sound_file):
     return result
 
 # check one example:
-# result = get_embeddings("/Users/liucija/repos/VGGish/data/audio/eval/car/-CZ1LIc8aos_20000_30000.wav")
+# model = get_vggish()
+# result = get_embeddings(model, "/Users/liucija/repos/VGGish/data/audio/unbal/car_passing_by/_-xwxCG8-iQ_30000_40000.wav")
 # result.shape
 # result
 
@@ -74,79 +76,82 @@ def dir_files(path):
     return [f for f in listdir(path) if isfile(join(path, f)) and not f.startswith('.')]
 
 
-def create_emb_data(directory):
+def create_emb_data(dataset, label):
     '''
-    Gets embeddings and puts them in one array for all files
+    Gets embeddings from audio files which are stored in
+    'data/audio/dataset/label/' for given dataset and label.
+    Files must already be downloaded and stored in this folder.
+    The embeddings are written to csv for individual file to save memory.
+
+    Keywords
+    --------
+    dataset: 'train', 'eval' or 'unbal'
+    label: any folder name that exists in data/audio/dataset
     '''
+    directory = 'data/audio/'+dataset+'/'+label+'/'
     files = dir_files(directory)
     # files = files[:2]  # limited when downloading
-    X = np.zeros((len(files), 128))
+    model = get_vggish()  # takes a while
+    # model.summary()
+
     for c, f in enumerate(files):
         print('Processing {} file out of {}'.format(c+1, len(files)))
-        embs = get_embeddings(directory+f)
+        embs = get_embeddings(model, directory+f)
+        newpath = 'data/embs/'+dataset+'/'+label+'/'
+        if not os.path.exists(newpath):
+            os.makedirs(newpath)
+        np.savetxt(newpath+'emb_'+str(c)+".csv", embs, delimiter=",")
+
+
+def combine_embs(dataset, label):
+    '''
+    Takes a name of the folder and combines embeddings available in
+    'data/embs/'
+
+    Keywords
+    --------
+    dataset: 'train', 'eval' or 'unbal'
+    label: any folder name that exists in data/embs/dataset
+    '''
+    directory = 'data/embs/'+dataset+'/'+label+'/'
+    files = dir_files(directory)
+    X = np.zeros((len(files), 128))
+
+    for c, f in enumerate(files):
+        print('Processing emb {} {}'.format(label, c+1))
+        embs = genfromtxt(directory+f, delimiter=',')
         X[c] = embs
+
+    np.savetxt('data/embs/'+dataset+'/'+label+'_embs.csv', X, delimiter=",")
 
     return X
 
 
-def combine_data(dataset):
+def make_dataset(dataset, pos_emb, neg_emb):
     '''
-    Obtains embeddings for thhe positive and negative class and puts them
-    into one array
-    ----
-    Keywords
-    - dataset: 'train' or 'eval'
     '''
-    car_embs = create_emb_data('data/audio/'+dataset+'/car/')
-    print('Car data shape: {}'.format(car_embs.shape))
-    print('Saving to .csv')
-    np.savetxt("data/embs/car_embs_"+dataset+".csv", car_embs, delimiter=",")
 
-    outdoor_embs = create_emb_data('data/audio/'+dataset+'/outside_rural/')
-    print('Outdoor data shape: {}'.format(outdoor_embs.shape))
-    print('Saving to .csv')
-    np.savetxt("data/embs/outdoor_embs_"+dataset+".csv", outdoor_embs, delimiter=",")
-
-    X = np.append(car_embs, outdoor_embs, 0)
+    X = np.append(pos_emb, neg_emb, 0)
     print('Final shape: {}'.format(X.shape))
-    print(X[0][0] == car_embs[0][0])
-    print(X[-1][0] == outdoor_embs[-1][0])
-    print('Saving to .csv')
+    print(X[0][0] == pos_emb[0][0])
+    print(X[-1][0] == neg_emb[-1][0])
+    print('Saving X to .csv')
     np.savetxt("data/embs/X_"+dataset+".csv", X, delimiter=",")
 
-    y = np.append(np.ones(len(car_embs)), np.zeros(len(outdoor_embs)))
-    print('Saving to .csv')
+    y = np.append(np.ones(len(pos_emb)), np.zeros(len(neg_emb)))
+    print('Saving y to .csv')
     np.savetxt("data/embs/y_"+dataset+".csv", y, delimiter=",")
 
     return X, y
 
-# Currently this function just stops for some reason on outdoor,
-# so broke it down into parts below
 
-X_train, y_train = combine_data('train')
-X_eval, y_eval = combine_data('eval')
+dataset = 'unbal'
+create_emb_data(dataset, 'car_passing_by')
+create_emb_data(dataset, 'outside_rural')
 
-# Broken down in steps:
+car_embs = combine_embs(dataset, 'car_passing_by')
+outside_embs = combine_embs(dataset, 'outside_rural')
 
-dataset = 'eval'  # then run for 'train'
+X, y = make_dataset(dataset, car_embs, outside_embs)
 
-car_embs = create_emb_data('data/audio/'+dataset+'/car/')
-print('Car data shape: {}'.format(car_embs.shape))
-print('Saving to .csv')
-np.savetxt("data/embs/car_embs_"+dataset+".csv", car_embs, delimiter=",")
-
-outdoor_embs = create_emb_data('data/audio/'+dataset+'/outside_rural/')
-print('Outdoor data shape: {}'.format(outdoor_embs.shape))
-print('Saving to .csv')
-np.savetxt("data/embs/outdoor_embs_"+dataset+".csv", outdoor_embs, delimiter=",")
-
-X = np.append(car_embs, outdoor_embs, 0)
-print(X[0][0] == car_embs[0][0])
-print(X[-1][0] == outdoor_embs[-1][0])
-print('Final shape: {}'.format(X.shape))
-print('Saving X to .csv')
-np.savetxt("data/embs/X_"+dataset+".csv", X, delimiter=",")
-
-y = np.append(np.ones(len(car_embs)), np.zeros(len(outdoor_embs)))
-print('Saving y to .csv')
-np.savetxt("data/embs/y_"+dataset+".csv", y, delimiter=",")
+#TODO: cleanup the multiple unnecessary file savings
